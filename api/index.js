5b324865d08cb8819b9e221780a60a7d229bcb29
Achintya1800 <achintya.chavan22@spit.ac.in>
2025-06-20 20:56:47 +0530

    create sign-up api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 const app = express();
 mongoose.connect(process.env.MONGO_URI)
@@ -12,8 +13,11 @@ mongoose.connect(process.env.MONGO_URI)
     console.log('Error connecting to MongoDB:', err);
   });
 
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log('Server is running on port 3000!!');
 });
 
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
