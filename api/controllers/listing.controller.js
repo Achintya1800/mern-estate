2f6c9a4737b666b4a2545b1079d5e07dd957c0ff
Achintya1800 <achintya.chavan22@spit.ac.in>
2025-06-22 22:35:22 +0530

    complete delete user listing functionality

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -14,7 +14,7 @@ export const deleteListing = async (req, res, next) => {
   const listing = await Listing.findById(req.params.id);
 
   if (!listing) {
-    return next(errorHandler(404, 'Listing not found!'));
+    return next(errorHandler(404, 'Listing noot found!'));
   }
 
   if (req.user.id !== listing.userRef) {
