1983447ab8ac81837e40085c5af287e11e4582cb
Achintya1800 <achintya.chavan22@spit.ac.in>
2025-06-23 11:32:15 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
 import listingRouter from './routes/listing.route.js';
+import path from 'path';
 
 dotenv.config();
 const app = express();
@@ -15,6 +16,9 @@ mongoose.connect(process.env.MONGO_URI)
   .catch((err) => {
     console.log('Error connecting to MongoDB:', err);
   });
+
+  const __dirname = path.resolve();
+
 
 app.use(express.json());
 
@@ -27,6 +31,12 @@ app.listen(3000, () => {
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 app.use((err,req,res,next) => {
   const statusCode = err.statusCode || 500;
